233a5fc93ff4cc492ae51d89555738454000642d
Lee Messenger <lee@n3o.co.uk>
2013-03-24 13:32:16 +0000

    Styling ajax spinner Making Polls visible to non logged in users

--- MVCForum.Website/Controllers/TopicController.cs
+++ MVCForum.Website/Controllers/TopicController.cs
@@ -308,12 +308,15 @@ namespace MVCForum.Website.Controllers
                     };
 
                     // See if the topic has a poll, and if so see if this user viewing has already voted
-                    if (topic.Poll != null && LoggedOnUser != null)
+                    if (topic.Poll != null)
                     {
                         // There is a poll and a user
                         // see if the user has voted or not
                         var votes = topic.Poll.PollAnswers.SelectMany(x => x.PollVotes).ToList();
-                        viewModel.UserHasAlreadyVotedInPoll = (votes.Count(x => x.User.Id == LoggedOnUser.Id) > 0);
+                        if (LoggedOnUser != null)
+                        {
+                            viewModel.UserHasAlreadyVotedInPoll = (votes.Count(x => x.User.Id == LoggedOnUser.Id) > 0);                            
+                        }
                         viewModel.TotalVotesInPoll = votes.Count();
                     }
 
