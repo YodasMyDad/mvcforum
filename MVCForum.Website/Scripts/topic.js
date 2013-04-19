874dcc12e8b5a022e3361eaf9d049a0b3e4adafa
Lee Messenger <lee@n3o.co.uk>
2013-04-19 20:05:01 +0100

    Styling the attachments Adding Fancybox to show for images uploaded

---
+++ MVCForum.Website/Scripts/topic.js
@@ -0,0 +1,5 @@
+﻿
+$(function () {
+    // Any images uploaded, fire fancybox
+    $('div.fileupload a[href$=".gif"], div.fileupload a[href$=".jpg"], div.fileupload a[href$=".png"], div.fileupload a[href$=".bmp"], div.fileupload a[href$=".jpeg"]').fancybox();
+});
