7458a1ade792e8567c663ec0700f36fa59ddf47a
Lee Messenger <lee@n3o.co.uk>
2013-04-23 12:19:12 +0100

    Fixed error when no examine index for first time topic Updated the CSV encoding to use Google docs as Excel doesn't save it correctly Fixed issue with post spinner not working Testing ready for v1.3 release

--- MVCForum.Website/Controllers/TopicController.cs
+++ MVCForum.Website/Controllers/TopicController.cs
@@ -458,10 +458,22 @@ namespace MVCForum.Website.Controllers
 
             // Get lucene to search, we are just searching on the topic name at the moment
             // Really need a more powerful lucene search for similar questions
-            var foundTopicIds = _luceneService.Search(formattedSearchTerm, string.Empty, AppConstants.SimilarTopicsListSize).Select(x => x.TopicId).ToList();
+            List<Topic> topics = null;
+            try
+            {
+                var searchResults = _luceneService.Search(formattedSearchTerm, string.Empty, AppConstants.SimilarTopicsListSize);
+                if (searchResults != null)
+                {
+                    var foundTopicIds = searchResults.Select(x => x.TopicId).ToList();
 
-            // Get the topics
-            var topics = _topicService.GetTopicsByCsv(AppConstants.SimilarTopicsListSize, foundTopicIds);
+                    // Get the topics
+                    topics = _topicService.GetTopicsByCsv(AppConstants.SimilarTopicsListSize, foundTopicIds).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                LoggingService.Error(ex);
+            }
 
             // Pass the list to the partial view
             return PartialView(topics);
