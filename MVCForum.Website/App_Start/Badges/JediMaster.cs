1118d3fbdc7bddb04d2d0148700865f869c7e57f
Lee Messenger <lee@aptitude.co.uk>
2014-12-24 10:52:00 +0000

    Added new Jedi Master Badge

---
+++ MVCForum.Website/App_Start/Badges/JediMaster.cs
@@ -0,0 +1,24 @@
+﻿using MVCForum.Domain.DomainModel;
+using MVCForum.Domain.DomainModel.Attributes;
+using MVCForum.Domain.Interfaces.API;
+using MVCForum.Domain.Interfaces.Badges;
+
+namespace MVCForum.Website.Badges
+{
+    [Id("4c54474b-51c2-4a52-bad2-96af5dea14d1")]
+    [Name("JediMasterBadge")]
+    [DisplayName("Badge.JediMasterBadge.Name")]
+    [Description("Badge.JediMasterBadge.Desc")]
+    [Image("jedi.png")]
+    [AwardsPoints(50)]
+    public class JediMasterBadge : IMarkAsSolutionBadge
+    {
+        public bool Rule(MembershipUser user, IMVCForumAPI api)
+        {
+            //Post is marked as the answer to a topic - give the post author a badge
+            var usersSolutions = api.Post.GetSolutionsWrittenByMember(user.Id);
+
+            return (usersSolutions.Count >= 50);
+        }
+    }
+}
