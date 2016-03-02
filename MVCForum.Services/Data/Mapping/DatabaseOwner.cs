c81282b2ea74a6251927cc5fe9cabbd801d43e0c
Michael Gerfen <gerfen@hotmail.com>
2016-03-01 16:09:03 -0800

    Added support to get the database owner name from <appSettings/>  using MVCForum:DatabaseOwner key  in web.config.  Defaults to "dbo" if the key is not present.

--- MVCForum.Services/Data/Mapping/DatabaseOwner.cs
+++ MVCForum.Services/Data/Mapping/DatabaseOwner.cs
@@ -1,13 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Configuration;
 
 namespace MVCForum.Services.Data.Mapping
 {
     internal static class DatabaseOwner
     {
-        public static readonly string Name = "Forum";
+        public static string Name
+        {
+            get
+            {
+                var owner = ConfigurationManager.AppSettings["MVCForum:DatabaseOwner"];
+                return string.IsNullOrEmpty(owner) ? "dbo" : owner;
+            }
+        }
     }
 }
