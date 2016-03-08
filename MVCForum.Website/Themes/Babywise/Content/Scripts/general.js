e0fdde93f35dbc6a5bedaefdb7f67b92397ec234
Michael Gerfen <gerfen@hotmail.com>
2016-03-08 09:39:30 -0800

    Began integration with Virto via custom theme and using a shared datbase schema.

---
+++ MVCForum.Website/Themes/Babywise/Content/Scripts/general.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    $.ajax({
+        url: '/babywise/themes/template/footer',
+        contentType: 'application/html; charset=utf-8',
+        type: 'GET',
+        dataType: 'html'
+    })
+    .success(function (result) {
+        $("#footer_container").html(result);
+    });
+});
+
