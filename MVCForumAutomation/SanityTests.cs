84d8844f72446ab5f2b84b6ad181929a44a204c2
arnonax <arnonaxelrod@hotmail.com>
2018-01-03 16:28:10 +0200

    use 'var'

--- MVCForumAutomation/SanityTests.cs
+++ MVCForumAutomation/SanityTests.cs
@@ -10,14 +10,14 @@ namespace MVCForumAutomation
         public void WhenARegisteredUserStartsADiscussionOtherAnonymousUsersCanSeeIt()
         {
             const string body = "dummy body";
-            LoggedInUser userA = MVCForum.RegisterNewUserAndLogin();
-            Discussion createdDiscussion = userA.CreateDiscussion(Discussion.With.Body(body));
+            var userA = MVCForum.RegisterNewUserAndLogin();
+            var createdDiscussion = userA.CreateDiscussion(Discussion.With.Body(body));
 
-            MVCForumClient anonymousUser = new MVCForumClient();
-            DiscussionHeader latestHeader = anonymousUser.LatestDiscussions.Top;
+            var anonymousUser = new MVCForumClient();
+            var latestHeader = anonymousUser.LatestDiscussions.Top;
             Assert.AreEqual(createdDiscussion.Title, latestHeader.Title,
                 "The title of the latest discussion should match the one we created");
-            Discussion viewedDiscussion = latestHeader.OpenDiscussion();
+            var viewedDiscussion = latestHeader.OpenDiscussion();
             Assert.AreEqual(body, viewedDiscussion.Body, 
                 "The body of the latest discussion should match the one we created");
         }
