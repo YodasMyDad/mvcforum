72e332957b781e68c344eea5586c19fd38b0affb
Lee Messenger <lee@aptitude.co.uk>
2018-01-06 08:36:50 +0000

    Hangfire added and setup

---
+++ MVCForum.Website/App_Start/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Hangfire;
+
+[assembly: OwinStartup(typeof(MvcForum.Web.Startup))]
+namespace MvcForum.Web
+{
+    using Core.Constants;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalConfiguration.Configuration.UseSqlServerStorage(SiteConstants.Instance.MvcForumContext);
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+        }
+    }
+}
