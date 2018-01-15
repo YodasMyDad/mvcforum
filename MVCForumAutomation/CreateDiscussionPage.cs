3b72ce7b0696b460ebd9e064df436c31d9bc8405
arnonax <arnonaxelrod@hotmail.com>
2018-01-15 16:47:32 +0200

    Implemented setters and methods in CreateDiscussionPage. Still not working because category was not selected.

--- MVCForumAutomation/CreateDiscussionPage.cs
+++ MVCForumAutomation/CreateDiscussionPage.cs
@@ -14,18 +14,36 @@ namespace MVCForumAutomation
         public string Title
         {
             get { throw new System.NotImplementedException(); }
-            set { throw new System.NotImplementedException(); }
+            set
+            {
+                var titleInput = _webDriver.FindElement(By.Id("Name"));
+                titleInput.SendKeys(value);
+            }
         }
 
         public string Body
         {
             get { throw new System.NotImplementedException(); }
-            set { throw new System.NotImplementedException(); }
+            set
+            {
+                var iframe = _webDriver.FindElement(By.Id("Content_ifr"));
+                _webDriver.SwitchTo().Frame(iframe);
+                try
+                {
+                    var htmlEditor = _webDriver.FindElement(By.Id("tinymce"));
+                    htmlEditor.SendKeys(value);
+                }
+                finally
+                {
+                    _webDriver.SwitchTo().ParentFrame();
+                }
+            }
         }
 
         public void CreateDiscussion()
         {
-            throw new System.NotImplementedException();
+            var submitButton = _webDriver.FindElement(By.CssSelector("[type=submit]"));
+            submitButton.Click();
         }
     }
 }
