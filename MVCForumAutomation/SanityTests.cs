98f4b3199ffbb4f61b243facaa4501f154b3e863
arnonax <arnonaxelrod@hotmail.com>
2018-01-21 21:02:28 +0200

    Adding MVCForum property, MVCForumClient class and LoggedInUser class

--- MVCForumAutomation/SanityTests.cs
+++ MVCForumAutomation/SanityTests.cs
@@ -6,14 +6,6 @@ namespace MVCForumAutomation
     [TestClass]
     public class SanityTests
     {
-        /*
-        Login as a registered user		                            // MVCForum (the application)
-        Start a discussion titled “Hi!” and body “dummy body” 		// Logged-in user
-        Enter the site as an anonymous user (from another browser)	// MVCForum (new instance)
-        Verify that a discussion titled “Hi!” appears       		// MVCForum.LatestDiscussions.Top (*)
-        Open that discussion                                		// Discussion header
-        Verify that the body of the discussion is “dummy body”		// Discussion
-             */
         [TestMethod]
         public void WhenARegisteredUserStartsADiscussionOtherAnonymousUsersCanSeeIt()
         {
@@ -29,5 +21,18 @@ namespace MVCForumAutomation
             Assert.AreEqual(body, viewedDiscussion.Body, 
                 "The body of the latest discussion should match the one we created");
         }
+
+        public MVCForumClient MVCForum
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+
+    public class MVCForumClient
+    {
+    }
+
+    public class LoggedInUser
+    {
     }
 }
