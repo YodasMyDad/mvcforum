aae341339de60ef9633af62e5ee478da2e8abd9e
Lee Messenger <lee@aptitude.co.uk>
2018-02-17 06:19:52 +0000

    Fixed BannedWordReplaceCharactor issue

--- MVCForum.Core/Utilities/CensorUtils.cs
+++ MVCForum.Core/Utilities/CensorUtils.cs
@@ -6,12 +6,12 @@
 
     public class CensorUtils
     {
-        public IList<string> CensoredWords { get; private set; }
+        public IList<string> CensoredWords { get; }
 
         public CensorUtils(IEnumerable<string> censoredWords)
         {
             if (censoredWords == null)
-                throw new ArgumentNullException("censoredWords");
+                throw new ArgumentNullException(nameof(censoredWords));
 
             CensoredWords = new List<string>(censoredWords);
         }
@@ -28,7 +28,6 @@
             foreach (var censoredWord in CensoredWords)
             {
                 var regularExpression = ToRegexPattern(censoredWord);
-
                 censoredText = Regex.Replace(censoredText, regularExpression, StarCensoredMatch, RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
             }
 
@@ -38,7 +37,12 @@
         private static string StarCensoredMatch(Match m)
         {
             var word = m.Captures[0].Value;
-            return new string(Convert.ToChar(ConfigUtils.GetAppSetting("BannedWordReplaceCharactor")), word.Length);
+            var bannedCharactor = ForumConfiguration.Instance.BannedWordReplaceCharactor;
+            if (!string.IsNullOrWhiteSpace(bannedCharactor))
+            {
+                return new string(Convert.ToChar(bannedCharactor), word.Length);
+            }
+            return string.Empty;
         }
 
         private static string ToRegexPattern(string wildcardSearch)
